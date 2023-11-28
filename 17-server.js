ac802b7800e772438dc12027c6cac2269cb1c345
s-kvng <kvngnathan8420@gmail.com>
2023-11-28 13:38:49 +0000

    add setInterval file and server file

---
+++ 17-server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("request event");
+  res.end("hello world");
+});
+
+server.listen(5000, () => {
+  console.log("Server listening to port: 5000...");
+});
