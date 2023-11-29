9e1ba99c53a141f83ff00cbe5b58929379dcde7d
s-kvng <kvngnathan8420@gmail.com>
2023-11-29 22:25:25 +0000

    clean fs-async code using util

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 //CLEANING THE FS-ASYNC CODE
 
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
-const getText = (path) => {
-  return new Promise((reject, resolve) => {
-    readFile(path, "utf8", (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/first.txt", "utf8");
+    const second = await readFilePromise("./content/second.txt", "utf8");
+    await writeFilePromise(
+      "./content/result-mind-grenade.txt",
+      `THIS IS AWESOME RIGHT:\n${first} \n${second}`
+    );
+    console.log("Done with the task");
+  } catch (error) {
+    console.log(`error : ${error}`);
+  }
 };
+
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, result) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(result);
+//       }
+//     });
+//   });
+// };
+
+// writeFile(
+//   "./content/result-async2.txt",
+//   `Here is the result of the clean fs-async code base :\n ${first}\n ${second}`,
+//   (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log("done with this task");
+//   }
+// );
