f47f25939b8309b264f6b0fec821f8f67c4d6d55
s-kvng <kvngnathan8420@gmail.com>
2023-11-29 22:28:11 +0000

    cleaner fs-async code base with promises directly to the require

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 //CLEANING THE FS-ASYNC CODE
 
-const { readFile, writeFile } = require("fs");
-const util = require("util");
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await readFilePromise("./content/first.txt", "utf8");
-    const second = await readFilePromise("./content/second.txt", "utf8");
-    await writeFilePromise(
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
       "./content/result-mind-grenade.txt",
-      `THIS IS AWESOME RIGHT:\n${first} \n${second}`
+      `THIS IS AWESOME RIGHT:\n${first} \n${second}`,
+      { flag: "a" }
     );
     console.log("Done with the task");
   } catch (error) {
