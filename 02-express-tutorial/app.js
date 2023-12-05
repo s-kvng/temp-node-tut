b6ead154ebeb6b65b9f54d69985aca3b4b511d84
s-kvng <kvngnathan8420@gmail.com>
2023-12-05 02:43:37 +0000

    params & query

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -15,6 +15,30 @@ app.get("/api/products", (req, res) => {
   res.json(newProduct);
 });
 
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req);
+  console.log(typeof req.params.productID);
+  const { productID } = req.params;
+  console.log(req.route.path);
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    res.status(404).send("<h1>Product does not exist </h1>");
+  }
+  res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("Hello world");
+});
+
+app.get("/api/v1/query", (req, res) => {
+  console.log(req.query);
+  res.send("Hello world");
+});
+
 app.listen(5000, () => {
   console.log("Server listening to port: 5000....");
 });
