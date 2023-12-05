b6d3531b69dc8d612b9dceb1bf6511267d2b4834
s-kvng <kvngnathan8420@gmail.com>
2023-12-05 16:52:56 +0000

    middleware use

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,7 +3,8 @@ const logger = require("./logger");
 const app = express();
 
 //  req => middleware => res
-app.use(logger); // if you want to apply the middleware to all , it must be at the top most level
+//we can also specifically define the routes to use the middleware on
+app.use("/api", logger); // if you want to apply the middleware to all , it must be at the top most level
 
 app.get("/", (req, res) => {
   res.send("Home");
@@ -11,6 +12,14 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.send("About");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
 });
 
 app.listen(5000, () => {
