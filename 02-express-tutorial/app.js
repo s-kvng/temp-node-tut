af1679fc9e208824810008475f81c703d3c48cec
s-kvng <kvngnathan8420@gmail.com>
2023-12-07 15:46:37 +0000

    refactor code to use express router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,28 +1,25 @@
 const express = require("express");
-const logger = require("./logger");
-const authorize = require("./authorize");
 const app = express();
 
-//  req => middleware => res
-//we can also specifically define the routes to use the middleware on
-app.use([logger, authorize]); // if you want to apply the middleware to all , it must be at the top most level
+const people = require("./routes/people");
 
-app.get("/", (req, res) => {
-  console.log(req.user);
-  res.send("Home");
-});
+//static files
+app.use(express.static("./methods-public"));
 
-app.get("/about", (req, res) => {
-  console.log(req.user);
-  res.send("About");
-});
+//parse form data
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/api/products", (req, res) => {
-  res.send("products");
-});
+//parse json
+app.use(express.json());
 
-app.get("/api/items", (req, res) => {
-  res.send("Items");
+app.use("/api/people", people);
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please provide credentials");
 });
 
 app.listen(5000, () => {
